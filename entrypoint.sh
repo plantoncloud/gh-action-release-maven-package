c8e107b148cedb841af657ae5520d1ebfcecce6a
Swarup Donepudi <swarup@Swarups-Mac-Studio.local>
2023-01-10 22:20:32 -0800

    added maven package version input

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ export PLANTON_CLOUD_SERVICE_CLIENT_SECRET=${3}
 export PLANTON_CLOUD_ARTIFACT_STORE_ID=${4}
 export PLANTON_CLOUD_PRODUCT_ID=${5}
 export PLANTON_CLOUD_ARTIFACT_STORE_MAVEN_REPO_URL=${6}
+export MAVEN_PACKAGE_VERSION=${7}
 
 if ! [ -n "${PLANTON_CLOUD_SERVICE_CLIENT_ID}" ]; then
   echo "PLANTON_CLOUD_SERVICE_CLIENT_ID is not set. Configure Machine Account Credentials for Repository or Organization."
@@ -30,6 +31,12 @@ if ! [ -n "${PLANTON_CLOUD_ARTIFACT_STORE_MAVEN_REPO_URL}" ]; then
   echo "PLANTON_CLOUD_ARTIFACT_STORE_MAVEN_REPO_URL is required. It should be set to the maven repository of the artifact-store on planton cloud"
   exit 1
 fi
+
+if ! [ -n "${MAVEN_PACKAGE_VERSION}" ]; then
+  echo "MAVEN_PACKAGE_VERSION is required. It should be set to the semantic version of the maven package to be used for the release"
+  exit 1
+fi
+
 
 #!/bin/bash
 set -o errexit
@@ -54,4 +61,5 @@ transformed_product_id=$(echo ${PLANTON_CLOUD_PRODUCT_ID} | tr '[:lower:]' '[:up
 export MAVEN_REPO_URL_$transformed_product_id=$PLANTON_CLOUD_ARTIFACT_STORE_MAVEN_REPO_URL
 #consumer projects of this action are expected to have a Makefile in the root of the repository and also
 # a target with name 'publish
+echo "releasing version: $MAVEN_PACKAGE_VERSION"
 make publish
